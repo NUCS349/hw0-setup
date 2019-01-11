9b06843266573a08cb52faad084b01967b6931d7
pseeth <prem@stark.local>
2019-01-10 17:28:42 -0800

    adding a dummy problem

---
+++ code.py
@@ -0,0 +1,2 @@
+def sum_numbers(x, y):
+    return None
--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import unittest
 
-class TestEnvironment(unittest.TestCase):
+class TestAssignment(unittest.TestCase):
     def test_environment(self):
         import numpy as np
+
+    def test_sum_numbers(self):
+        from code import sum_numbers
+        self.assertEqual(sum_numbers(3, 5), 8)
+        self.assertEqual(sum_numbers(5, 4), 9)
+        self.assertEqual(sum_numbers(-1, 3), 2)
 
 if __name__ == '__main__':
     suite = (unittest.TestLoader()
